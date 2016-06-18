ccfbc8867b6b4acd535acbe53f5a64b0e9ef3a27
Thomas Nys <nysthee@gmail.com>
2016-06-18 22:12:37 +0200

    User BookItem in ListView

--- Components/App.js
+++ Components/App.js
@@ -1,47 +1,116 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import BookItem from './BookItem';
+import config from '../config';
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    paddingTop: 24,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  list: {
+    flex: 1,
+    flexDirection: 'row',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  listContent: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    fontSize: 24,
+    padding: 42,
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+  },
+  sectionDivider: {
+    padding: 8,
+    backgroundColor: '#EEEEEE',
+    alignItems: 'center',
+  },
+  headingText: {
+    flex: 1,
+    fontSize: 24,
+    alignSelf: 'center',
   },
 });
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([]),
+    };
+  }
+
+  componentDidMount() {
+    this.refreshData();
+  }
+
+  refreshData() {
+    fetch(`http://api.nytimes.com/svc/books/lists/hardcover-fiction?response-format%20=json&api-key=${config.ny_times_api_key}`)
+      .then(response => response.json())
+      .then((rjson) => {
+        console.log(rjson);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(rjson.body),
+        });
+      }
+    )
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  renderHeader() {
+    return (
+      <View style={styles.sectionDivider}>
+        <Text style={styles.headingText}>
+          Bestsellers in Hardcover Fiction
+        </Text>
+      </View>);
+  }
+
+  renderFooter() {
+    return (
+      <View style={styles.sectionDivider}>
+        <Text>Data from the New York Times bestsellers list.</Text>
+      </View>);
+  }
+
+  renderRow(rowData) {
+    return (
+      <BookItem
+        style={{ marginTop: 24 }}
+        coverURL={rowData.book_details[0].book_image}
+        title={rowData.book_details[0].title}
+        author={rowData.book_details[0].author}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        renderHeader={this.renderHeader}
+        renderFooter={this.renderFooter}
+      />
+    );
+  }
+}
 
 export default App;
